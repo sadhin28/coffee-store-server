fdc7141bee87a97d430c0a6502b1b97b8309db9a
sadhin28 <taosifsadhin113@gmail.com>
2025-06-24 22:52:17 +0600

    delete operation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //middlewire
 app.use(cors());
 app.use(express.json())
@@ -46,6 +46,13 @@ async function run() {
             res.send(result)
         })
 
+        // Delete operation
+        app.delete('/coffee/:id',async(req,res)=>{
+            const id=req.params.id
+            const query ={_id: new ObjectId(id)}
+            const result=await coffeecollection.deleteOne(query)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
