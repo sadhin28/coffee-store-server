c1c2bbdd4ddb331fe7291456f63780c439e2ac0a
sadhin28 <taosifsadhin113@gmail.com>
2025-06-24 23:11:15 +0600

    add update on e operation

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+        //Update Operation
+        app.get('/coffee/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query={_id: new ObjectId(id)}
+            const result = await coffeecollection.findOne(query)
+            res.send(result)
+        })
        
         app.post('/coffee', async (req, res) => {
             const newcoffee = req.body;
